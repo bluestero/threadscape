dd08246e6aabfce41054280e8ab04fb76fb425ee
Ahmed Khatib <ahmedkhatib99@gmail.com>
2024-12-11 00:54:30 +0530

    Updated the cursor effect.

--- threadscape/static/scripts/cursor.js
+++ threadscape/static/scripts/cursor.js
@@ -1,30 +1,7 @@
 const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".cursor-circle");
 
-const colors = [
-  "#ffb56b",
-  "#fdaf69",
-  "#f89d63",
-  "#f59761",
-  "#ef865e",
-  "#ec805d",
-  "#e36e5c",
-  "#df685c",
-  "#d5585c",
-  "#d1525c",
-  "#c5415d",
-  "#c03b5d",
-  "#b22c5e",
-  "#ac265e",
-  "#9c155f",
-  "#950f5f",
-  "#830060",
-  "#7c0060",
-  "#680060",
-  "#60005f",
-  "#48005f",
-  "#3d005e"
-];
+const colors = ["#ffffff"];
 
 circles.forEach(function (circle, index) {
   circle.x = 0;
